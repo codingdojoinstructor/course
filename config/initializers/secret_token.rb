4d43c87c9096aa7251b3797f26badd015e2e410c
frisk <friskslalom6@Randalls-MacBook-Pro-2.local>
2013-07-11 15:18:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Courses::Application.config.secret_token = '878ce0e2c1c0e370a6d8fa9a8627cba5aeb4c99a55fff01f960eb32c90c5a592a613f0398483aa94cae8fe6cf010db1f2b4911ab9331c1d1abdcd4cf7e7168f7'
